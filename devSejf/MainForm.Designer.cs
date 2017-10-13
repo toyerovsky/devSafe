296756614ae659a03c0591014394e46f75bcd601
Przemysław Postrach <toyerek@gmail.com>
2017-10-13 17:05:06 +0200

    Add project files.

---
+++ devSejf/MainForm.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace devSejf
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.startButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.okLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 10;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 41);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 10;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(602, 409);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(12, 8);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(664, 613);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "START";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            this.startButton.BackColor = System.Drawing.Color.DarkGray;
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 100F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.startButton.ForeColor = System.Drawing.Color.Green;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(9, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 3;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(65, 12);
+            this.progressBar1.Maximum = 1000;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(549, 13);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // okLabel
+            // 
+            this.okLabel.AutoSize = true;
+            this.okLabel.Location = new System.Drawing.Point(621, 13);
+            this.okLabel.Name = "okLabel";
+            this.okLabel.Size = new System.Drawing.Size(0, 13);
+            this.okLabel.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 624);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = " © Toyer and his cat 2016";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(688, 643);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.okLabel);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "devSejf alpha 0.1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label okLabel;
+        public System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ devSejf/MainForm.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace devSejf
+{
+    public partial class MainForm : Form
+    {
+        public String ToClick { get; set; }
+
+        int i = 0;
+
+        Random random = new Random();
+
+        List<Control> controls = new List<Control>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            timer1.Interval = 10;
+
+            ToClick = random.Next(100).ToString();
+            label1.Text = ToClick;
+   
+
+            for (int i = 0; i < 100; i++)
+            {
+                Button button = new Button();
+                button.Visible = true;
+                button.Name = button.Name + i.ToString();
+                button.BackColor = System.Drawing.Color.DarkGray;
+                button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+                button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+                button.ForeColor = System.Drawing.Color.Green;
+                button.Size = new System.Drawing.Size(60, 50);
+                button.Text = i.ToString();
+
+                button.Click += delegate (System.Object o, System.EventArgs ea)
+                {
+                    if (button.Text == ToClick)
+                    {
+                        NextNumber();
+                    }
+                };
+
+                controls.Add(button);
+            }
+        }
+
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            tableLayoutPanel1.Visible = true;
+
+            Shuffle(tableLayoutPanel1);
+
+            startButton.Visible = false;
+
+            timer1.Start();
+        }
+
+        //Aby skorzystać z metody kontener musi mieć już przypisane kontrolki
+
+        public void Shuffle(TableLayoutPanel panel)
+        {
+            IEnumerable<Control> query = controls.OrderBy(p => random.Next(101));
+            
+            tableLayoutPanel1.Controls.Clear();
+
+            foreach (var control in query)
+            {
+                panel.Controls.Add(control);
+            }
+        }
+
+        public void NextNumber()
+        {
+            if (i < 3)
+            {
+                timer1.Stop();
+                ToClick = random.Next(100).ToString();
+                label1.Text = ToClick;
+                progressBar1.Value = 0;
+                i++;
+
+                okLabel.Text += "✓ ";
+
+                Shuffle(tableLayoutPanel1);
+                timer1.Start();
+            }
+            else
+            {
+                timer1.Stop();
+                progressBar1.Value = 0;
+                startButton.Visible = true;
+                Shuffle(tableLayoutPanel1);
+                okLabel.Text = "";
+                i = 0;
+                MessageBox.Show("Gratuluje otworzyłeś sejf.");
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (progressBar1.Value < 1000)
+            {
+                progressBar1.Value++;
+            }
+            else
+            {
+                timer1.Stop();
+                Shuffle(tableLayoutPanel1);
+                progressBar1.Value = 0;
+                ToClick = random.Next(100).ToString();
+                label1.Text = ToClick;
+                timer1.Start();
+            }
+        }
+    }
+}
